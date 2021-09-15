2528695a043b919b0c246bcbeb3316c84c2aa536
SHowardGal <u0250173@utah.edu>
2021-09-14 20:59:54 -0600

    commit

---
+++ Python Create .py
@@ -0,0 +1,27 @@
+
+(base) C:\Users\steph>python
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] :: Anaconda, Inc. on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello World!"
+... print("Hello World!")
+  File "<stdin>", line 2
+    print("Hello World!")
+    ^
+SyntaxError: invalid syntax
+>>> print('Hello World'!')
+  File "<stdin>", line 1
+    print('Hello World'!')
+                       ^
+SyntaxError: invalid syntax
+>>> print('Hello World')
+Hello World
+>>> PI = 3.14
+>>> PI
+3.14
+>>> first_number = int(input('Type the first number: ')) ;\
+... second_number = int(input('Type the second number: ')) ;\
+... print("The sum is: ", first_number + second_number)
+Type the first number: 2
+Type the second number: 3
+The sum is:  5
+>>>
